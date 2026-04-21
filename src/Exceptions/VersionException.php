9893e81d6b9dfa103572c989d0294cc1e396748f
Roland Edi <rolandedi@codivoire.com>
2026-04-21 19:56:39 +0000

    refactor(core): restructure codebase into src directory and add framework components
    - Move core files from root to src/ directory
    - Update composer.json autoload PSR-4 mapping to "Horizom\\Core\\": "src/"
    - Add new core classes: App, Config, Container, View, ServiceProvider
    - Introduce contracts: ExceptionHandler, FacadeContract, Responsable
    - Add exceptions: VersionException
    - Implement facades: Config, Facade, Request, Response, View
    - Add middlewares: BodyParsingMiddleware, ContentLengthMiddleware, ExceptionHandlingMiddleware
    - Add service providers: ConfigServiceProvider, CoreServiceProvider, ExceptionServiceProvider, ViewServiceProvider
    - Add helper functions in helpers.php
    - Update dependencies in composer.json (dispatcher, http, routing to ^5.2, phpunit to ^11.0, etc.)
    - Update README.md badges to use proper links
    - Add MIT LICENSE file
    - Modify .gitignore to include composer.phar and composer.lock
    
    BREAKING CHANGE: Major version update to 4.0.0 with structural reorganization and new dependencies requiring PHP 8.0+

--- src/Exceptions/VersionException.php
+++ src/Exceptions/VersionException.php
@@ -7,7 +7,7 @@ class VersionException extends \Exception
     public function __construct(string $message = null, $code = 0, \Exception $previous = null)
     {
         parent::__construct(
-            "This version of Horizom requires at least PHP 8.0 but you are currently running PHP " . explode('-', PHP_VERSION)[0] . ". Please update your PHP version.", 
+            $message ?? "This version of Horizom requires at least PHP 8.0 but you are currently running PHP " . explode('-', PHP_VERSION)[0] . ". Please update your PHP version.",
             $code,
             $previous
         );
